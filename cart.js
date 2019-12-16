6c178c593dfb49a523a00a661ee3c956152d6461
breburdaTom <36457709+breburdaTom@users.noreply.github.com>
2019-12-16 21:54:21 +0100

    first part of solution

--- cart.js
+++ cart.js
@@ -1,18 +1,39 @@
-// Amazon shopping
 const user = {
-    name: 'Kim',
+    name: "Kim",
     active: true,
     cart: [],
-    purchases: []
-  }
+    purchases: [],
+    addToCart: function(item) {
+      user.cart.push(item);
+    },
+    buyItem: function(item) {
+      for (var i = 0; i < this.cart.length; i++) {
+        this.purchases.push(this.cart[i]);
+        this.cart.splice(i, 1);
+        i--; //decrement i IF we remove an item
+      }
+    },
+    emptyCart: function() {
+      for (var i = 0; i < this.cart.length; i++) {
+        this.cart.splice(i, 1);
+        i--;
+      }
+    }
+  };
   
+  const item1 = {
+    name: "Phone",
+    price: 100
+  };
+  
+  const item2 = {
+    name: "Watch",
+    price: 250
+  };
   
   //Implement a cart feature:
-  // 1. Add items to cart.
+  // 1. Add items to cart. DONE
   // 2. Add 3% tax to item in cart
-  // 3. Buy item: cart --> purchases
-  // 4. Empty cart
+  // 3. Buy item: cart --> purchases DONE
+  // 4. Empty cart DONE
   
-  //Bonus:
-  // accept refunds.
-  // Track user history.
